691ef1c49c8f73cf7b86fe62b95857d5320774fe
rajatdhoot <rajat.dhoot@mountblue.io>
2017-06-24 16:01:00 +0530

    Made Modal Transparent and Responsive

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,13 @@
 $(function(){
     console.log("Working");
-    $( ".img-responsive" ).click(function() {
-  $(".modal , .pop").css({"display" : "block"});
+    $( ".image1" ).click(function() {
+  $(".modal , .pop1, .popup").css({"display" : "block"});
+});
+    $( ".image2" ).click(function() {
+  $(".modal , .pop2, .popup").css({"display" : "block"});
+});
+    $( ".image3" ).click(function() {
+  $(".modal , .pop3, .popup").css({"display" : "block"});
 });
     $( ".modal" ).click(function() {
     location.reload();
